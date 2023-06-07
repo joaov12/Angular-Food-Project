603ddbf77636194bf8de73bb712a71aa22676d35
Joao Vitor <jvfs12th@gmail.com>
2023-06-07 15:53:39 -0300

    Descrição das imagens + estilização

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
-
+import { Food } from '../shared/models/Food';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,15 +8,11 @@ import { FoodService } from '../services/food/food.service';
 })
 export class HomeComponent implements OnInit {
 
-
-  foods:String[] = [];
-
-  constructor(private foodService:FoodService){
-
-  }
+  foods:Food[] = [];
+  constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
-      this.foods = this.foodService.getAll();
+    this.foods = this.foodService.getAll();
   }
 
 }
